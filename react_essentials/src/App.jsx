494f3c6cb371254ebbd2e71814b45cba4ddd5197
SHEMNDARONGUGI <ndaroshem@gmail.com>
2026-04-08 23:13:55 +0300

    First Lesson

---
+++ react_essentials/src/App.jsx
@@ -0,0 +1,41 @@
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function AddNumbers(a, b) {
+  return (
+    <h3>
+      The sum of {a} and {b} is: {a + b} and{" "}
+    </h3>
+  );
+}
+
+function Header() {
+  const description = reactDescriptions[genRandomInt(2)];
+
+  return (
+    <header>
+      {/* <img src="src/assets/react-core-concepts.png" alt="Stylized atom" /> */}
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
